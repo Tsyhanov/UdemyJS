de09d02ea81135a90db3cf32eed4cfccdd35bfec
Oleksandr Tsyhanov <tsyhanov@gmail.com>
2022-06-10 22:28:36 +0300

    Lesson 44. Practice. Tasks 1 to 3

---
+++ 44_Practice/js/script.js
@@ -0,0 +1,38 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+console.log("test")
+//1
+var adv = document.getElementsByClassName("promo__adv")
+adv[0].remove()
+//2
+var genre = document.getElementsByClassName("promo__genre")
+genre[0].textContent = "Drama"
+//3
+var bg = document.getElementsByTagName("body")
+console.log(bg[0])
+bg[0].setAttribute("background", "img/bg.jpg")
+//4
